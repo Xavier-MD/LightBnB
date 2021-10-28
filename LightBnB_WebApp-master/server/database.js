3e05fbf5b167ca43d2cbc72f5367525e826a1267
Xavier Martin Demers <xavier.pmd@outlook.com>
2021-10-27 22:41:13 -0400

    Added funcitonality to the getAllReservations function.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -17,11 +17,12 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  return pool.query(`
-  SELECT *
-  FROM users
-  WHERE email = $1
-  `, [email])
+  return pool
+    .query(`
+    SELECT *
+    FROM users
+    WHERE email = $1
+    `, [email])
     .then((result) => {
       if (result) {
         return (result.rows[0]);
@@ -38,11 +39,12 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return pool.query(`
-  SELECT *
-  FROM users
-  WHERE id = $1
-  `, [id])
+  return pool
+    .query(`
+    SELECT *
+    FROM users
+    WHERE id = $1
+    `, [id])
     .then((result) => {
       if (result) {
         return (result.rows[0]);
@@ -50,7 +52,7 @@ const getUserWithId = function(id) {
         return null;
       }
     });
-}
+};
 exports.getUserWithId = getUserWithId;
 
 
@@ -60,11 +62,12 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  return pool.query(`
+  return pool
+    .query(`
     INSERT INTO users (name, email, password)
     VALUES ($1, $2, $3)
     RETURNING *;
-  `, [user.name, user.email, user.password])
+    `, [user.name, user.email, user.password])
     .then((result) => {
       return (result.rows[0]);
     })
@@ -82,7 +85,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+    SELECT *
+    FROM reservations
+    JOIN properties ON properties.id = property_id
+    WHERE guest_id = $1
+    LIMIT $2;
+    `, [guest_id, limit])
+    .then((result) => {
+      if (result) {
+        return (result.rows);
+      } else {
+        return null;
+      }
+    });
 };
 exports.getAllReservations = getAllReservations;
 
